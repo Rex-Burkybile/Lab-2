65dffb4008b220449bda69e463039856bce808d9
0800490 <0800490@ACIS-TUTOR07-P.pittstate.edu>
2019-09-16 11:44:24 -0500

    Assignment complete

--- src/main/java/homework/lab2/BarChart.java
+++ src/main/java/homework/lab2/BarChart.java
@@ -33,7 +33,103 @@ import javax.swing.JOptionPane;
 public class BarChart {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        JOptionPane.showMessageDialog(null, "Enter a postive nonzero number.");
+        
+        boolean i = true;
+        int storeSales1AsInt = 0;
+                
+        while(i==true) {
+        JOptionPane.showMessageDialog(null, "Enter todays sales for the five stores.");
+        String storeSales1AsString = JOptionPane.showInputDialog("First store's sales today");
+        storeSales1AsInt = Integer.parseInt(storeSales1AsString);
+        if (storeSales1AsInt > 0) {
+            i = false;
+        }
+        else { 
+            JOptionPane.showMessageDialog(null, "Please enter a positive number.");
+        }
+        }
+        System.out.print("Store 1: ");
+       int sales1 = storeSales1AsInt/100;
+        for (int v = 0; v<sales1 ; v++){
+            System.out.print("*");
     }
-  
+        boolean j = true;
+        int storeSales2AsInt = 0;
+                
+        while(j==true) {
+        String storeSales2AsString = JOptionPane.showInputDialog("Second store's sales today");
+        storeSales2AsInt = Integer.parseInt(storeSales2AsString);
+        if (storeSales2AsInt > 0) {
+            j = false;
+        }
+        else { 
+            JOptionPane.showMessageDialog(null, "Please enter a positive number.");
+        }
+        }
+        System.out.println("");
+        System.out.print("Store 2: ");
+       int sales2 = storeSales2AsInt/100;
+        for (int v = 0; v<sales2 ; v++){
+            System.out.print("*");
+        }
+        boolean k = true;
+        int storeSales3AsInt = 0;
+                
+        while(k==true) {
+        String storeSales3AsString = JOptionPane.showInputDialog("Third store's sales today");
+        storeSales3AsInt = Integer.parseInt(storeSales3AsString);
+        if (storeSales3AsInt > 0) {
+            k = false;
+        }
+        else { 
+            JOptionPane.showMessageDialog(null, "Please enter a positive number.");
+        }
+        }
+        System.out.println("");
+        System.out.print("Store 3: ");
+       int sales3 = storeSales3AsInt/100;
+        for (int v = 0; v<sales3 ; v++){
+            System.out.print("*");
+        }
+        boolean l = true;
+        int storeSales4AsInt = 0;
+                
+        while(l==true) {
+        String storeSales4AsString = JOptionPane.showInputDialog("Forth store's sales today");
+        storeSales4AsInt = Integer.parseInt(storeSales4AsString);
+        if (storeSales4AsInt > 0) {
+            l = false;
+        }
+        else { 
+            JOptionPane.showMessageDialog(null, "Please enter a positive number.");
+        }
+        }
+        System.out.println("");
+        System.out.print("Store 4: ");
+       int sales4 = storeSales4AsInt/100;
+        for (int v = 0; v<sales4 ; v++){
+            System.out.print("*");
+        }
+        boolean m = true;
+        int storeSales5AsInt = 0;
+                
+        while(m==true) {
+        String storeSales5AsString = JOptionPane.showInputDialog("Fifth store's sales today");
+        storeSales5AsInt = Integer.parseInt(storeSales5AsString);
+        if (storeSales5AsInt > 0) {
+            m = false;
+        }
+        else { 
+            JOptionPane.showMessageDialog(null, "Please enter a positive number.");
+        }
+        }
+        System.out.println("");
+        System.out.print("Store 5: ");
+       int sales5 = storeSales5AsInt/100;
+        for (int v = 0; v<sales5 ; v++){
+            System.out.print("*");
+        }
+    }
 }
+
+
